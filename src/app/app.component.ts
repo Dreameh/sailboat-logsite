2e1606b7ada80ab343d1e0318ff01e9b3b1a1fda
dreameh <dreameh@localhost-live.lan>
2020-03-19 14:48:32 +0200

    Fixed working HttpClient fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import testJson from './_files/testJson.json';
+import { AppService } from './app.service';
+import { SailboatList } from './model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
 
-  title = 'sailboat-log';
-  public testList = testJson;
-  public positionList = testJson.position;
+export class AppComponent {
+	sailboat: SailboatList;
+
+	title = 'sailboat-log';
+	constructor(private appService: AppService) {
+		// this.sailboat = this.appService.getSailboat()
+		// .subscribe((data: SailboatList) => this.sailboat = { ...data },
+		// 		  error => this.error = error);
+
+		this.appService.getSailboat().subscribe(res => { this.sailboat = res; });
+	}
 }
