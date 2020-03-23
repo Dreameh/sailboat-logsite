2e69966ae63c878b8f6dd70915fdb893a11fd01b
Lucas Pentinsaari <kyusaki1@gmail.com>
2020-03-23 10:53:33 +0200

    removed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { WindComponent } from './wind/wind.component';
+// import { WindComponent } from './wind/wind.component';
 
 
 @NgModule({
