267ac81eca79f50adbe68bb00badadb0e7ceace9
Lucas Pentinsaari <kyusaki1@gmail.com>
2020-04-08 09:05:21 +0300

    fixed coords

--- src/app/coords/coords.component.ts
+++ src/app/coords/coords.component.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { CoordsService } from './coords.service';
+import { Coords } from './Coords';
 
 @Component({
   selector: 'app-coords',
@@ -8,7 +10,18 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 
 export class CoordsComponent {
+  private coord: Coords;
+  constructor() {}
+
   coordForm = new FormGroup({
     latitude: new FormControl(''),
     longitude: new FormControl('')});
+
+  // this.coordsService.addCoords(coord)
+  // .subscribe(cord => this.heroes.push(cord));
+
+  onSubmit() {
+    this.coord = this.coordForm.value;
+    console.warn(this.coord);
+  }
 }
